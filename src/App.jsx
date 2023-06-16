87a493c72d9e0ab7fc4e88d14ff2682646c4307c
aswathy999773 <aswathysuresh251@gmail.com>
2023-06-16 15:57:45 +0530

    change todolist code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { useState, useEffect} from 'react';
 
-function App() {
+export default function App() {
+  const [todos,setTodo] = useState([]);
+   
+    useEffect(() => {
+      getData();
+    
+    },[]);
+       const getData =async () => {
+        try{
+          const incomingData=await fetch(
+            "https://jsonplaceholder.typicode.com/todos"
+          );
+          const formattedData =await incomingData.json();
+          setTodo(formattedData);
+          console.log(formattedData);
+        }catch(error) {
+          console.log(error);
+        }
+       };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <container className="my-5">
+        <h1 className="my-5 text-center display-2">Todo List</h1>
+      <Table striped bordered hover variant="dark">
+         
+      <thead>
+      <tr>
+          <td>id</td>
+          <td>Title</td>
+          <td>Completed</td>
+         </tr>
+      </thead>
+      <tbody>
+      
+         {todos.map((todo,i) =>(
+          <tr key={todo.id}>
+            <td>{i+1}</td>
+            <td  className={todo.completed? 'text-success' :'text-danger'}>{todo.title}</td>
+            <td>{todo.completed ? 'Yes':'No'}</td>
+          </tr>
+         ))}
+      </tbody>
+    </Table>
+    </container>
     </div>
   );
 }
-
-export default App;
